e669d907009a9cd1e4a71d3fd18d3095f030f76a
Rui Guerreiro <145457706+Guerreiro-13@users.noreply.github.com>
2023-09-29 09:53:14 +0100

    Add files via upload

--- JavaScript/Variaveis-de-cada-tipo/script.js
+++ JavaScript/Variaveis-de-cada-tipo/script.js
@@ -1,5 +1,5 @@
 
-var ultimoNome = "Guerreiro";
+var ultimoNome = 'Guerreiro';
 //console.log(" Tipo String - " & ultimoNome);
 console.log(ultimoNome)
 
@@ -12,3 +12,9 @@ var resultadoCerto = true;
 //Boolean(resultadoCerto)
 console.log(typeof(resultadoCerto))
 //console.log(" Tipo Boolean - " & resultadoCerto);
+
+const pi = 3.14;
+console.log(pi)
+
+const code = 'Trusted';
+console.log(code)
